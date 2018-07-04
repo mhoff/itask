d5607a8da41561cc529297b528fee01681e20d63
Michael Hoff <mail@michael-hoff.net>
2018-07-04 15:27:58 +0200

    convert package from distutils to setuptools

--- setup.py
+++ setup.py
@@ -1,23 +1,42 @@
-from distutils.core import setup
+from setuptools import setup, find_packages
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setup(
     name="itask",
-    packages=["itask"],
-    version="0.1.5",
-    description="Interactive shell for taskwarrior",
+    version="0.1.6",
+    packages=find_packages(),
+    install_requires=[
+        'markdown',  # TODO necessary?
+        'prompt_toolkit>=1.0.15<=2.0.3',
+    ],
     author="Michael Hoff",
     author_email="mail@michael-hoff.net",
+    description="Interactive shell for taskwarrior",
+    license="GPL-3.0",  # TODO correct formatting?
+    keywords="taskwarrior task task-manager interactive shell",
     url="https://github.com/mhoff/itask",
     entry_points={
         'console_scripts': [
             'itask=itask:main',
         ],
     },
-    install_requires=[
-        'prompt_toolkit>=1.0.15,<=2.0.3',
-    ],
+    package_data={
+        '': ['*.md'],  # TODO proper way to handle READMEs?
+    },
     long_description=long_description,
+    classifiers=[
+        'Development Status :: 4 - Beta'
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)'
+        'Topic :: Utilities'
+    ],
+    project_urls={
+        "Bug Tracker": "https://github.com/mhoff/itask/issues",
+        # TODO 'Documentation' should point to documentation specific to the module *version*
+        "Documentation": "https://github.com/mhoff/itask/blob/master/README.md",
+        "Source Code": "https://github.com/mhoff/itask",
+    },
 )
